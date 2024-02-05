d25198fb9e2c74c45904205f2eb1e3efdc525ccd
zHd4 <computersystembase@gmail.com>
2024-02-06 00:38:56 +0100

    Update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,17 +3,41 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.parseInt(port);
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
+            if (!isProduction()) {
+                config.bundledPlugins.enableDevLogging();
+            }
         });
 
-        app.get("/", ctx -> ctx.result("Hello World"));
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
 
         return app;
     }
 
     public static void main(String[] args) {
-        getApp().start(7070);
+        getApp().start(getPort());
     }
+
 }
