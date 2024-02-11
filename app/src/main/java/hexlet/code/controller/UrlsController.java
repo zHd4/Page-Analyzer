d1955ddcaff254fdf10290450df0054632f87992
zHd4 <computersystembase@gmail.com>
2024-02-11 22:59:10 +0100

    Add UrlsController

---
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -0,0 +1,13 @@
+package hexlet.code.controller;
+
+import io.javalin.http.Context;
+
+public class UrlsController {
+    public static void index(Context ctx) {
+
+    }
+
+    public static void create(Context ctx) {
+
+    }
+}
