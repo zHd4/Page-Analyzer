57730a029fdd7e3fc61de8ea50315570c29e5240
zHd4 <computersystembase@gmail.com>
2024-02-11 22:58:50 +0100

    Add RootController

---
+++ app/src/main/java/hexlet/code/controller/RootController.java
@@ -0,0 +1,9 @@
+package hexlet.code.controller;
+
+import io.javalin.http.Context;
+
+public class RootController {
+    public static void index(Context ctx) {
+        ctx.render("index.jte");
+    }
+}
