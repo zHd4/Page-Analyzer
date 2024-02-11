f78a3353c717477c7f06e59c19b5915a9c4aac4d
zHd4 <computersystembase@gmail.com>
2024-02-11 22:44:47 +0100

    Add NamedRoutes

---
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -0,0 +1,11 @@
+package hexlet.code.util;
+
+public final class NamedRoutes {
+    public static String rootPath() {
+        return "/";
+    }
+
+    public static String urlsPath() {
+        return "/urls";
+    }
+}
