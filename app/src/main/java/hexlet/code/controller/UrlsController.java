0b0df31341c6c168a97fce66a5cce1e80d66abf4
zHd4 <computersystembase@gmail.com>
2024-02-12 23:56:10 +0100

    Implement UrlsController

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -1,13 +1,54 @@
 package hexlet.code.controller;
 
+import hexlet.code.dto.urls.UrlsPage;
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlsRepository;
 import io.javalin.http.Context;
 
-public class UrlsController {
-    public static void index(Context ctx) {
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
+public class UrlsController {
+    public static void index(Context ctx) throws SQLException {
+        List<Url> urls = UrlsRepository.getEntities();
+        UrlsPage page = new UrlsPage(urls);
+
+        page.setFlashText(ctx.consumeSessionAttribute("flash-text"));
+        page.setFlashType(ctx.consumeSessionAttribute("flash-type"));
+
+        ctx.render("urls.jte", Collections.singletonMap("page", page));
     }
 
-    public static void create(Context ctx) {
+    public static void create(Context ctx) throws SQLException {
+        String url = ctx.formParam("url");
 
+        List<Url> urls = UrlsRepository.getEntities();
+        UrlsPage page = new UrlsPage(urls);
+
+        try {
+            URI uri = new URI(Objects.requireNonNull(url));
+            url = String.format("%s://%s", uri.getScheme(), uri.getAuthority());
+
+            if (UrlsRepository.findByName(url).isEmpty()) {
+                UrlsRepository.save(new Url(url, Timestamp.from(Instant.now())));
+
+                page.setFlashText("Страница успешно добавлена");
+                page.setFlashType("success");
+            } else {
+                page.setFlashText("Страница уже существует");
+                page.setFlashType("primary");
+            }
+        } catch (URISyntaxException | NullPointerException e) {
+            page.setFlashText("Некорректный URL");
+            page.setFlashType("danger");
+        }
+
+        ctx.render("urls.jte", Collections.singletonMap("page", page));
     }
 }
