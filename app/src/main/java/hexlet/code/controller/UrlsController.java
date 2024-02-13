2f288df74f7415b14b4f9bed221b11e8f3940f2e
zHd4 <computersystembase@gmail.com>
2024-02-13 21:25:40 +0100

    Fix UrlsController

--- app/src/main/java/hexlet/code/controller/UrlsController.java
+++ app/src/main/java/hexlet/code/controller/UrlsController.java
@@ -3,6 +3,7 @@ package hexlet.code.controller;
 import hexlet.code.dto.urls.UrlsPage;
 import hexlet.code.model.Url;
 import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.http.Context;
 
 import java.net.URI;
@@ -28,9 +29,6 @@ public class UrlsController {
     public static void create(Context ctx) throws SQLException {
         String url = ctx.formParam("url");
 
-        List<Url> urls = UrlsRepository.getEntities();
-        UrlsPage page = new UrlsPage(urls);
-
         try {
             URI uri = new URI(Objects.requireNonNull(url));
             url = String.format("%s://%s", uri.getScheme(), uri.getAuthority());
@@ -38,17 +36,17 @@ public class UrlsController {
             if (UrlsRepository.findByName(url).isEmpty()) {
                 UrlsRepository.save(new Url(url, Timestamp.from(Instant.now())));
 
-                page.setFlashText("Страница успешно добавлена");
-                page.setFlashType("success");
+                ctx.sessionAttribute("flash-text", "Страница успешно добавлена");
+                ctx.sessionAttribute("flash-type", "success");
             } else {
-                page.setFlashText("Страница уже существует");
-                page.setFlashType("primary");
+                ctx.sessionAttribute("flash-text", "Страница уже существует");
+                ctx.sessionAttribute("flash-type", "primary");
             }
         } catch (URISyntaxException | NullPointerException e) {
-            page.setFlashText("Некорректный URL");
-            page.setFlashType("danger");
+            ctx.sessionAttribute("flash-text", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
         }
 
-        ctx.render("urls.jte", Collections.singletonMap("page", page));
+        ctx.redirect(NamedRoutes.urlsPath());
     }
 }
