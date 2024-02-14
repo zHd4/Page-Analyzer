d35ff1d3b254a0d3a5d73352f72bca87defdca15
zHd4 <computersystembase@gmail.com>
2024-02-14 02:41:44 +0100

    Update NamedRoutes

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -8,4 +8,16 @@ public final class NamedRoutes {
     public static String urlsPath() {
         return "/urls";
     }
+
+    public static String urlPath() {
+        return "/urls/{id}";
+    }
+
+    public static String urlPath(long id) {
+        return urlPath(String.valueOf(id));
+    }
+
+    public static String urlPath(String id) {
+        return "/urls/" + id;
+    }
 }
