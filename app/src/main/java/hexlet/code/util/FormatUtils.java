7900aa56347631814f5d545113a8a57a177bdc2a
zHd4 <computersystembase@gmail.com>
2024-02-14 02:27:59 +0100

    Add FormatUtils

---
+++ app/src/main/java/hexlet/code/util/FormatUtils.java
@@ -0,0 +1,12 @@
+package hexlet.code.util;
+
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public final class FormatUtils {
+    public static String formatTimestamp(Timestamp timestamp) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        return formatter.format(timestamp.toLocalDateTime());
+    }
+}
