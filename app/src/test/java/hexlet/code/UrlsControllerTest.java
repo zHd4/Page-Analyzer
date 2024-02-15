80123b521922a3af05e322be28c7820b24348e65
zHd4 <computersystembase@gmail.com>
2024-02-15 20:31:30 +0100

    Add UrlsControllerTest

---
+++ app/src/test/java/hexlet/code/UrlsControllerTest.java
@@ -0,0 +1,21 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class UrlsControllerTest {
+    @BeforeAll
+    public static void init() {
+
+    }
+
+    @Test
+    void testAdding() {
+
+    }
+
+    @Test
+    void testShowing() {
+
+    }
+}
