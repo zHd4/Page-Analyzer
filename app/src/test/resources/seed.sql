92022b2a04cbca185032b418bb9924175e519afb
zHd4 <computersystembase@gmail.com>
2024-02-17 00:58:15 +0100

    Add seed.sql

---
+++ app/src/test/resources/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO urls (id, name, created_at)
+VALUES (1, 'https://github.com', '17.02.2024, 00:40');
+
+INSERT INTO urls (id, name, created_at)
+VALUES (2, 'https://google.com', '17.02.2024, 00:40');
+
+INSERT INTO urls (id, name, created_at)
+VALUES (3, 'https://microsoft.com', '17.02.2024, 00:40');
