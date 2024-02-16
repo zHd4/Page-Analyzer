4504b1f62fec5d8d0b4364e502be6c2c87fd17f4
zHd4 <computersystembase@gmail.com>
2024-02-17 00:57:38 +0100

    Add Resources

---
+++ app/src/main/java/hexlet/code/util/Resources.java
@@ -0,0 +1,22 @@
+package hexlet.code.util;
+
+import hexlet.code.App;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public final class Resources {
+    public static String readResourceFile(String fileName) throws IOException {
+        InputStream inputStream = Objects.requireNonNull(App.class.getClassLoader().getResourceAsStream(fileName));
+        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+        try (BufferedReader reader = new BufferedReader(streamReader)) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+}
