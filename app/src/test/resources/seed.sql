ec03ee13fdc6cd885269c008fa53f935c5989f00
zHd4 <computersystembase@gmail.com>
2024-02-17 21:56:34 +0100

    Remove seed.sql, truncate.sql

--- app/src/test/resources/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO urls (id, name, created_at)
-VALUES (1, 'https://github.com', '2024-02-17 00:40:12.491151');
-
-INSERT INTO urls (id, name, created_at)
-VALUES (2, 'https://google.com', '2024-02-17 00:41:06.273015');
-
-INSERT INTO urls (id, name, created_at)
-VALUES (3, 'https://microsoft.com', '2024-02-17 00:41:51.817112');
