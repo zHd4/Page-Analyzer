e928aa45ad13f565ea667be81b1731a4946c0836
zHd4 <computersystembase@gmail.com>
2024-02-17 01:07:05 +0100

    Fix seed.sql

--- app/src/test/resources/seed.sql
+++ app/src/test/resources/seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO urls (id, name, created_at)
-VALUES (1, 'https://github.com', '17.02.2024, 00:40');
+VALUES (1, 'https://github.com', '2024-02-17 00:40:12.491151');
 
 INSERT INTO urls (id, name, created_at)
-VALUES (2, 'https://google.com', '17.02.2024, 00:40');
+VALUES (2, 'https://google.com', '2024-02-17 00:41:06.273015');
 
 INSERT INTO urls (id, name, created_at)
-VALUES (3, 'https://microsoft.com', '17.02.2024, 00:40');
+VALUES (3, 'https://microsoft.com', '2024-02-17 00:41:51.817112');
