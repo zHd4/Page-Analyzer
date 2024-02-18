7c60e1a9c043fc20315c73f1ca47dfe9350df34f
zHd4 <computersystembase@gmail.com>
2024-02-18 20:34:26 +0100

    Update AppTest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,14 @@
 package hexlet.code;
 
 import hexlet.code.model.Url;
+import hexlet.code.repository.BaseRepository;
 import hexlet.code.repository.UrlsRepository;
 import io.javalin.Javalin;
 import kong.unirest.HttpResponse;
 import kong.unirest.HttpStatus;
 import kong.unirest.Unirest;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +17,7 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static hexlet.code.util.FormatUtils.formatTimestamp;
 
 public final class AppTest {
     private static final int PORT = 0;
@@ -32,6 +35,23 @@ public final class AppTest {
     @AfterAll
     public static void afterAll() {
         app.stop();
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        BaseRepository.runScript("DELETE FROM urls;");
+        BaseRepository.runScript("ALTER TABLE urls ALTER COLUMN id RESTART WITH 4;");
+
+        BaseRepository.runScript("""
+                INSERT INTO urls (id, name, created_at)
+                VALUES (1, 'https://github.com', '2024-02-17 00:40:12.491151');
+
+                INSERT INTO urls (id, name, created_at)
+                VALUES (2, 'https://google.com', '2024-02-17 00:41:06.273015');
+
+                INSERT INTO urls (id, name, created_at)
+                VALUES (3, 'https://microsoft.com', '2024-02-17 00:41:51.817112');""");
+
     }
 
     @Test
@@ -64,7 +84,18 @@ public final class AppTest {
     }
 
     @Test
-    void testShowing() {
+    void testShowing() throws SQLException {
+        long id = 1;
+        Optional<Url> optionalUrl = UrlsRepository.findById(id);
 
+        HttpResponse<String> response = Unirest.get(baseUrl + "/urls/" + id).asString();
+        String body = response.getBody().replace("\n", "");
+
+        assertThat(optionalUrl.isEmpty()).isFalse();
+        Url url = optionalUrl.get();
+
+        assertThat(body).contains(String.format("<td>%s</td>", url.getId()));
+        assertThat(body).contains(String.format("<td>%s</td>", url.getName()));
+        assertThat(body).contains(String.format("<td>%s</td>", formatTimestamp(url.getCreatedAt())));
     }
 }
